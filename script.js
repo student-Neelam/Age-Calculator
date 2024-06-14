67d3ee94ab6579910526a39086e3abc6972affac
student-Neelam <kannojianeelam7@gmail.com>
2024-06-14 14:03:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function calculateAge() {
+
+    var birthdate = document.getElementById('birthdate').value;
+
+    var today = new Date();
+
+    var birthDate = new Date(birthdate);
+
+    var age = today.getFullYear() - birthDate.getFullYear();
+
+    var month = today.getMonth() - birthDate.getMonth();
+ 
+   
+    
+if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+ 
+       age--;
+
+}
+
+document.getElementById('result').innerHTML = "You are " + age + " years old.";
+
+}
